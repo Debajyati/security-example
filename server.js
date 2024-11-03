59d004bba7739f8a2ad5113d3b6ea097349ffd17
Debajyati Dey <ddebajyati@gmail.com>
2024-11-03 23:20:12 +0530

    changed the connection protocol from http to https

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 
 const PORT = 3000;
 
 const app = express();
 
+app.get('/secret', (req,res) => {
+  return res.send('Your Personal Secret value is 42!');
+})
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
